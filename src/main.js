a241a511fac9dbcd7572c89a1aaf745ce46feced
Noel Kirkland <noelkirkland@gmail.com>
2020-07-03 09:59:14 -0700

    confirm now that I am receiving an error statement from the api call

--- src/main.js
+++ src/main.js
@@ -3,7 +3,17 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { getExchangeRates } from './../src/currency-exchange.js';
+// import { getConversion } from './../src/currency-exchange.js';
 
+async function getElements(){
+  const response = await getExchangeRates();
+  if (response === false){
+    $(".show-exchange-rate").text(`There was an error handling your request.`);
+  } else {
+    console.log(response.result);
+    $(".show-exchange-rate").text("The exchange rate from USD to is " + response.conversion_rates);
+  }
+}
 
 
 $(document).ready(function () {
@@ -16,15 +26,7 @@ $(document).ready(function () {
     $("#ammount-usd").val("");
     $("#country").val("");
 
-
-    async function getElements(){
-      const response = await getExchangeRates();
-      if (response === false){
-        $(".show-exchange-rate").text(`There was an error handling your request.`);
-      } else {
-        $(".show-exchange-rate").text("The exchange rate from USD to " + amountUSD + " is " + response.time_last_update_unix + currency);
-      }
-    }
+    $(".show-exchange-rate").text("The exchange rate from USD to " + amountUSD + " is " + currency);
     getElements()
   });
 });
